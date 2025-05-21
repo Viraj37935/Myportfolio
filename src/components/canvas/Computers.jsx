958b138fe79da06d795a3f9f2de5f90f784d0580
viraj37935 <virajdimble6@gmail.com>
2025-05-21 23:49:43 +0530

    work

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -71,7 +71,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <OrbitControls enableRotate={!isMobile} enableZoom={false} />
+        <OrbitControls enableRotate={!isMobile} enableZoom={true} />
         {/* Show Model */}
         <Computers isMobile={isMobile} />
       </Suspense>
