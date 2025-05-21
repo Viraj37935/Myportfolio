270b878603c45d9c912c3ee6b16f131c40b75947
viraj37935 <virajdimble6@gmail.com>
2025-05-21 23:45:13 +0530

    work

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,9 @@ const Hero = () => {
       </div>
 
       {/* Computer Model */}
-      <ComputersCanvas />
+      <div className="canvas-wrapper">
+        <ComputersCanvas />
+      </div>
 
       {/* Scroll to about section */}
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
