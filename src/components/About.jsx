0e0f43733570061b4b7c190c74bcf9e3914f947d
viraj37935 <virajdimble6@gmail.com>
2025-05-22 22:18:18 +0530

    Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Tilt } from 'react-tilt';
- // Modify the import to include "/tilt"
+import { Tilt } from "react-tilt";
+// Modify the import to include "/tilt"
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -49,11 +49,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="empty-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled website developer with experience in Javascript, 
-        and expertise in frameworks like React, Node.js
-        . I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled website developer with experience in Javascript, and
+        expertise in frameworks like React, Node.js . I'm a quick learner and
+        collaborate closely with clients to create efficient, scalable, and
+        user-friendly solutions that solve real-world problems. Let's work
+        together to bring your ideas to life!
       </motion.p>
 
       {/* Service Card */}
