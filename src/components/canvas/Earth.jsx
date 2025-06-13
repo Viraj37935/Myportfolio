85bd5297f1d2140bce4deadd88f407f81138f95e
viraj37935 <virajdimble6@gmail.com>
2025-06-13 16:24:58 +0530

    work

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,18 +1,15 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
-import CanvasLoader from "../Loader";
-
-// Earth component
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive
+      object={earth.scene}
+      scale={2.8}
+      position={[0, -1, 0]}
+      rotation-y={0}
+    />
   );
 };
 
-// Earth canvas container
 const EarthCanvas = () => {
   return (
     <div className="earth-canvas-container">
@@ -20,9 +17,11 @@ const EarthCanvas = () => {
         shadows
         frameloop="demand"
         gl={{ preserveDrawingBuffer: true }}
-        camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+        camera={{ fov: 50, near: 0.1, far: 200, position: [0, 1, 6] }}
       >
         <Suspense fallback={<CanvasLoader />}>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[5, 5, 5]} intensity={1} />
           <OrbitControls
             autoRotate
             enableZoom={false}
@@ -36,5 +35,3 @@ const EarthCanvas = () => {
     </div>
   );
 };
-
-export default EarthCanvas;
